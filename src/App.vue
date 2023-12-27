cab5682ddd0f1508f5f97efd2f00a86e831b4eee
BoyChai <1972567225@qq.com>
2023-12-27 19:06:31 +0800

    Update Layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="common-layout" v-if="loginStatus">
+    <el-container>
+      <el-header id="header" class="bg_color" style=" height: 120px; display: flex; align-items: center;">
+        <div class="margins">
+          <h1 style="float: left;">山化大树洞</h1>
+          <div class="navigation bg_color">
+            <span class="bg_color">我要登陆</span>
+            <span class="bg_color">我要投稿</span>
+            <span class="bg_color">首页</span>
+          </div>
+        </div>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer style="background-color:#F2F5F7;height: 100px; display: flex; align-items: center;position: fixed;left: 0px;bottom: 0px;right: 0px;">
+        <div style="background-color:#F2F5F7;margin-left: 6%">
+          版权所有 © 2023 - 山东化工职业学院 的 计算机社团
+        </div>
+      </el-footer>
+    </el-container>
+  </div>
+
 </template>
 
 <style>
@@ -27,4 +43,27 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+* {
+  margin: 0;
+  padding: 0;
+  background-color: #FAFBFC;
+}
+#header>*{
+  background-color: white;
+}
+.margins{
+  margin: 0 6%;width: 100%;
+}
+.bg_color {
+  background-color: white
+}
+.navigation>span {
+  margin-left: 20px;
+  float: right;
+}
 </style>
+<script setup>
+import {ref} from "vue";
+
+const loginStatus = ref(true)
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+
+const app= createApp(App)
+
+// ElementPlus加载
+app.use(ElementPlus)
+
+app.use(router).mount('#app')
